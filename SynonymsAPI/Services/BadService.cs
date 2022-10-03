200466233f57876a1390fe19cc4d0c9e08131b60
Irma <irma.karasoftic@gmail.com>
2022-10-03 14:22:47 +0200

    worsening

--- SynonymsAPI/Services/BadService.cs
+++ SynonymsAPI/Services/BadService.cs
@@ -63,5 +63,85 @@
                 return false;
             }
         }
+
+        public bool Add(string word, string foo)
+        {
+            try
+            {
+                if (word == "A")
+                {
+
+                }
+                else if (word.Contains("B"))
+                {
+                    if (word == "BA")
+                    {
+
+                    }
+                    else if (word == "BB")
+                    {
+
+                    }
+                }
+                else if (word == "C")
+                {
+
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Add(string word, int foo)
+        {
+            try
+            {
+                if (word == "A")
+                {
+                    if (word == "A")
+                    {
+
+                    }
+                    else if (word.Contains("B"))
+                    {
+                        if (word == "BA")
+                        {
+
+                        }
+                        else if (word == "BB")
+                        {
+
+                        }
+                    }
+                    else if (word == "C")
+                    {
+
+                    }
+                }
+                else if (word.Contains("B"))
+                {
+                    if (word == "BA")
+                    {
+
+                    }
+                    else if (word == "BB")
+                    {
+
+                    }
+                }
+                else if (word == "C")
+                {
+
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
     }
 }
