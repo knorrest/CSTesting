e75a3ce52ea457a12358145027c4d28200b1ccd0
Irma <irma.karasoftic@gmail.com>
2022-10-03 14:20:15 +0200

    worsening the code

--- SynonymsAPI/Services/BadService.cs
+++ SynonymsAPI/Services/BadService.cs
@@ -2,6 +2,66 @@
 {
     public class BadService
     {
+        public bool Add(string word)
+        {
+            try
+            {
+                if (word == "A")
+                {
 
+                }
+                else if (word.Contains("B"))
+                {
+                    if (word == "BA")
+                    {
+
+                    }
+                    else if (word == "BB")
+                    {
+
+                    }
+                }
+                else if (word == "C")
+                {
+
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Add(string word, bool foo)
+        {
+            try
+            {
+                if (word == "A")
+                {
+
+                }
+                else if (word.Contains("B"))
+                {
+                    if (word == "BA")
+                    {
+
+                    }
+                    else if (word == "BB")
+                    {
+
+                    }
+                }
+                else if (word == "C")
+                {
+
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
     }
 }
