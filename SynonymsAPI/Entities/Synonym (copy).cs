e49d7477874bdf8cf5a9fc8923557aa30da34cea
Irma <irma.karasoftic@gmail.com>
2022-10-24 09:36:48 +0200

    DUMMY-333: test bug fix

---
+++ SynonymsAPI/Entities/Synonym (copy).cs
@@ -0,0 +1,10 @@
+﻿namespace SynonymsDB
+{
+    public class Synonym
+    {
+        public long Id { get; set; }
+        public string SynonymString { get; set; }
+        public long WordId { get; set; }
+        public Word Word { get; set; }
+    }
+}
