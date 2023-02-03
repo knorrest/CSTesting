a3cd5f7fb0d6cd16feafd7481937fc8835351d63
Irma <irma.karasoftic@gmail.com>
2023-02-03 09:12:27 +0100

    BUG-123: added overloaded functions

--- SynonymsAPI/Services/BadService.cs
+++ SynonymsAPI/Services/BadService.cs
@@ -320,7 +320,47 @@ namespace SynonymsAPI.Services
             return result;
         }
 
-        public decimal Calculate5(decimal amount, int type, int years)
+        public decimal Calculate(decimal amount, int type, int years, bool foo)
+        {
+            decimal result = 0;
+            decimal disc = (years > 5) ? (decimal)5 / 100 : (decimal)years / 100;
+            if (type == 1)
+            {
+                result = amount;
+                if (type == 1)
+                {
+                    result = amount;
+                }
+                else if (type == 2)
+                {
+                    result = (amount - (0.1m * amount)) - disc * (amount - (0.1m * amount));
+                }
+                else if (type == 3)
+                {
+                    result = (0.7m * amount) - disc * (0.7m * amount);
+                }
+                else if (type == 4)
+                {
+                    result = (amount - (0.5m * amount)) - disc * (amount - (0.5m * amount));
+                }
+                return result;
+            }
+            else if (type == 2)
+            {
+                result = (amount - (0.1m * amount)) - disc * (amount - (0.1m * amount));
+            }
+            else if (type == 3)
+            {
+                result = (0.7m * amount) - disc * (0.7m * amount);
+            }
+            else if (type == 4)
+            {
+                result = (amount - (0.5m * amount)) - disc * (amount - (0.5m * amount));
+            }
+            return result;
+        }
+
+        public decimal Calculate(decimal amount, int type, int years, bool foo1, bool foo2)
         {
             decimal result = 0;
             decimal disc = (years > 5) ? (decimal)5 / 100 : (decimal)years / 100;
