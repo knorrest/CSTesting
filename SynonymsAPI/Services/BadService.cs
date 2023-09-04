37ae9677fa9a32777361fc6a8bd1528cfc243357
Irma <irma.karasoftic@gmail.com>
2023-09-04 12:42:07 +0200

    worsen the file

--- SynonymsAPI/Services/BadService.cs
+++ SynonymsAPI/Services/BadService.cs
@@ -485,6 +485,46 @@ namespace SynonymsAPI.Services
             }
         }
 
+        public decimal Calculate(decimal amount, int type, int years, bool foo1, bool foo2, bool foo3)
+        {
+            decimal result = 0;
+            decimal disc = (years > 5) ? (decimal)5 / 100 : (decimal)years / 100;
+            if (type == 1)
+            {
+                result = amount;
+                if (type == 1)
+                {
+                    result = amount;
+                }
+                else if (type == 2)
+                {
+                    result = (amount - (0.1m * amount)) - disc * (amount - (0.1m * amount));
+                }
+                else if (type == 3)
+                {
+                    result = (0.7m * amount) - disc * (0.7m * amount);
+                }
+                else if (type == 4)
+                {
+                    result = (amount - (0.5m * amount)) - disc * (amount - (0.5m * amount));
+                }
+                return result;
+            }
+            else if (type == 2)
+            {
+                result = (amount - (0.1m * amount)) - disc * (amount - (0.1m * amount));
+            }
+            else if (type == 3)
+            {
+                result = (0.7m * amount) - disc * (0.7m * amount);
+            }
+            else if (type == 4)
+            {
+                result = (amount - (0.5m * amount)) - disc * (amount - (0.5m * amount));
+            }
+            return result;
+        }
+
         public decimal Calculate(decimal amount, int type, int years)
         {
             decimal result = 0;
